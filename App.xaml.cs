135266c2ed8c399abe3c3510b9017f89102f1241
Kamvitae Surimushado <kamvitae@gmail.com>
2024-01-02 22:09:13 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HF_ch10_SaveHumans_Again
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
